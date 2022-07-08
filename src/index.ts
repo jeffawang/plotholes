6a8faa1c21753afab1a99823112b482308feb205
Jeff Wang <jeffawang@gmail.com>
2022-07-07 20:18:28 -0700

    schotter example. press s to save as svg

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,73 @@ import p5svg from "p5.js-svg";
 p5svg(p5);
 
 const sketch = function (p: p5) {
-    const CELL_COUNT = 20;
 
-    const rect = (x: number, y: number, width: number, color: p5.Color) => {
-        const cellWidth = width / CELL_COUNT;
+    const WIDTH = 900;
+    const HEIGHT = 1200;
 
-        for (let col = 0; col < CELL_COUNT; col++) {
-            for (let row = 0; row < CELL_COUNT; row++) {
-                const alpha = p.noise(row, col) * 255;
-                color.setAlpha(alpha);
-                p.fill(color);
-                p.rect(x + cellWidth * row, y + cellWidth * col, cellWidth, cellWidth);
-            }
-        }
-    }
+    const ROWS = 15;
+    const COLS = 11;
+
+    const MARGIN = 100;
+
+    const JITTER = 0.02;
+    const SHIFT_FACTOR = 1.5;
+
+    const COLORS = {
+        BG: p.color(240),
+        FG: p.color(0),
+    };
+
     p.setup = function () {
         // NOTE(jw): p.SVG gets imperitively added by p5svg, IDE may not understand it
-        p.createCanvas(420, 420, p.SVG);
+        p.createCanvas(WIDTH, HEIGHT, p.SVG);
         p.noLoop();
         p.noStroke();
+        p.background(COLORS.BG);
+
+        const seed = Math.floor(p.random(0, Number.MAX_SAFE_INTEGER));
+        p.randomSeed(seed);
+        p.noiseSeed(seed);
+        console.log(seed);
     }
 
     p.draw = function () {
-        p.background(0);
-        const color = p.color('red');
-        rect(0, 0, 420, color);
+
+        p.noFill();
+        p.stroke(COLORS.FG);
+        p.strokeWeight(2);
+        p.rectMode("center");
+
+        const size = (WIDTH - 2 * MARGIN) / COLS;
+
+        p.translate(MARGIN, MARGIN);
+
+        for (let y = 0; y < ROWS; y++) {
+            for (let x = 0; x < COLS; x++) {
+                const yness = y / ROWS + 0.2;
+                const dy = p.pow(p.random(-yness, yness), 3) * size * SHIFT_FACTOR;
+                const dx = p.pow(p.random(-yness, yness), 3) * size * SHIFT_FACTOR;
+                const theta = p.random(-p.PI, p.PI) * p.pow(yness, 3);
+                // console.log(dy);
+                p.push();
+
+                // move to top left of grid cell, then its center.
+                p.translate(x * size, y * size);
+                p.translate(size / 2, size / 2);
+
+                p.rotate(theta + p.random(-JITTER, JITTER));
+                // p.point(0, 0);
+                p.rect(dx, dy, size);
+                p.pop();
+            }
+        }
+        // p.save();
     };
+    p.keyPressed = function () {
+        if (p.key == 's') {
+            p.save();
+        }
+    }
 };
 
 // export const makeArt = (node: HTMLElement) => {
