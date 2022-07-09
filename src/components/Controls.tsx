4f57efe5dda9a8675acce0238fc5012c177af06f
Jeff Wang <jeffawang@gmail.com>
2022-07-09 16:43:13 -0700

    tidy up a bit and add comment

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,98 +2,7 @@ import { Heading } from '@chakra-ui/react';
 import GroupControlComponent from "./Group";
 import RadioControlComponent from "./Radio";
 import SliderControlComponent from "./Slider";
-import { SketchControl, Uniform, Uniforms } from "./Types";
-
-let exampleControls: SketchControl[] = [
-    {
-        type: "group",
-        name: "control group 1",
-        children: [
-            {
-                type: "slider",
-                name: "a slider in group 1",
-                uniform: "stuff",
-                defaultValue: 0.5
-            },
-            {
-                type: "group",
-                name: "a nested group",
-                children: [
-                    {
-                        type: "slider",
-                        name: "a slider in the nested group",
-                        uniform: "stuff",
-                        defaultValue: 0.5
-                    },
-                    {
-                        type: "slider",
-                        name: "another slider in the nested group",
-                        uniform: "stuff2",
-                        defaultValue: 0.75
-                    },
-                ]
-            },
-        ]
-    },
-    {
-        type: "slider",
-        name: "a groupless slider",
-        uniform: "stuff2",
-        defaultValue: 0.75
-    },
-    {
-        type: "radio",
-        name: "a radio button",
-        options: [
-            {
-                name: "option 1",
-                value: "option1"
-            },
-            {
-                name: "option 2",
-                value: "option2"
-            },
-            {
-                name: "option 3",
-                value: "option3"
-            },
-        ],
-        defaultValue: "option1",
-        uniform: "omg"
-    },
-]
-
-function uniformsToControls(uniforms: Uniforms): SketchControl[] {
-    return Object.keys(uniforms).map((name) => {
-        const u = uniforms[name];
-        switch (u.type) {
-            case 'slider':
-                return {
-                    type: "slider",
-                    name: name,
-                    uniform: name,
-                    defaultValue: u.value,
-                    step: u.step,
-                    min: u.min,
-                    max: u.max,
-                }
-            case 'radio':
-                return {
-                    type: "radio",
-                    name: name,
-                    uniform: name,
-                    defaultValue: u.value,
-                    options: u.options.map((v) => ({ name: v, value: v }))
-                }
-            case 'group':
-                return {
-                    type: "group",
-                    name: name,
-                    children: [] // TODO
-                }
-        }
-    });
-}
+import { Uniform } from "./Types";
 
 export function subControl(name: string, uniform: Uniform) {
     switch (uniform.type) {
@@ -109,9 +18,9 @@ export function subControl(name: string, uniform: Uniform) {
 function ControlsComponent({ name, uniforms }) {
     return <div style={{ "padding": "10px" }}>
         <Heading>{name}</Heading>
+        {/* Keys are ordered enough in modern ES
+         https://stackoverflow.com/questions/60802305/typescript-ordered-dictionary */}
         {Object.keys(uniforms).map((name) => subControl(name, uniforms[name]))}
-        {/* {uniformsToControls(uniforms).map(subControl)} */}
-        {/* {exampleControls.map(subControl)} */}
     </div>;
 }
 
