7b539aa2986797d92adf1b9118be861a784150f2
Jeff Wang <jeffawang@gmail.com>
2022-07-09 10:26:03 -0700

    add tsdocs

--- src/sketcher.ts
+++ src/sketcher.ts
@@ -2,11 +2,27 @@ import p5 from "p5";
 import p5svg from "p5.js-svg";
 p5svg(p5);
 
+/**
+ * Params holds the parameters surrounding the sketch that may also interact
+ * with things outside the sketch, such as width, height, controls, and the
+ * sketch function itself. This is the main configuration interface into
+ * Sketcher.
+ */
 type Params = {
     title: string
     width: number
     height: number
     controls: Controls
+
+    /**
+     * sketch is the main way to access p5 for Sketcher users. Inside this
+     * closure, users can do all the normal p5 things with the p argument, and
+     * the Sketcher instance is passed through to provide access to Params,
+     * Controls, and default functions.
+     * 
+     * @param p - a p5 object
+     * @param s - a Sketcher object to provide access to Params, Controls, etc.
+     */
     sketch: (p: p5, s: Sketcher) => void
 
     seed?: number
@@ -26,6 +42,14 @@ class Sketcher {
         this.params = params;
     }
 
+    /**
+     * setup is used inside of Sketcher to set the default sketch setup
+     * function. Users of Sketcher can call it inside their sketch closure to
+     * extend it if necessary.
+     * 
+     * @param p - a p5 object, typically passed through from a p5 sketch closure.
+     * @returns a setup closure that can be set on the p5 sketch directly.
+     */
     setup(p: p5) {
         return () => {
             // @ts-ignore NOTE(jw): p.SVG gets imperitively added by p5svg, IDE may not understand it, so ts-ignore it.
@@ -40,6 +64,14 @@ class Sketcher {
         }
     }
 
+    /**
+     * keyPressed is used inside of Sketcher to set the default sketch
+     * keyPressed function. Users of Sketcher can call it inside their sketch
+     * closure to extend it if necessary.
+     * 
+     * @param p - a p5 object, typically passed through from a p5 sketch closure.
+     * @returns a setup closure that can be set on the p5 sketch directly.
+     */
     keyPressed(p: p5) {
         return () => {
             switch (p.key) {
@@ -57,6 +89,12 @@ class Sketcher {
         }
     }
 
+    /**
+     * setDefaults defaults the keyPressed and setup functions for the given p5
+     * instance if the user did not specify them in the sketch closure.
+     * 
+     * @param p - a p5 object, typically passed through from a p5 sketch closure.
+     */
     setDefaults(p: p5) {
         if (p.keyPressed === undefined)
             p.keyPressed = this.keyPressed(p).bind(this)
@@ -64,6 +102,12 @@ class Sketcher {
             p.setup = this.setup(p).bind(this);
     }
 
+    /**
+     * p5Sketch returns a closure to give to p5, while making the Sketcher
+     * instance available to the user inside our sketch closure.
+     * 
+     * @returns a sketch function suitable to pass to the p5 library.
+     */
     p5Sketch() {
         return (p: p5) => {
             this.params.sketch(p, this);
