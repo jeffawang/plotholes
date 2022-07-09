ca6e589f2828e886ba0de467d6475743aeb5c6d9
Kevin Reynolds <tossrock@gmail.com>
2022-07-08 21:27:57 -0700

    First pass at a circuitry inspired pattern

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,6 @@ const sketch = function (p: p5) {
         p.noLoop();
         p.noStroke();
 
-
         p.randomSeed(SEED);
         p.noiseSeed(SEED);
         console.log(SEED);
@@ -45,28 +44,119 @@ const sketch = function (p: p5) {
         p.rectMode("center");
 
         const size = (WIDTH - 2 * MARGIN) / COLS;
-
         p.translate(MARGIN, MARGIN);
-
-        for (let y = 0; y < ROWS; y++) {
-            for (let x = 0; x < COLS; x++) {
-                const yness = y / ROWS + 0.2;
-                const dy = p.pow(p.random(-yness, yness), 3) * size * SHIFT_FACTOR;
-                const dx = p.pow(p.random(-yness, yness), 3) * size * SHIFT_FACTOR;
-                const theta = p.random(-p.PI, p.PI) * p.pow(yness, 3);
-                // console.log(dy);
-                p.push();
-
-                // move to top left of grid cell, then its center.
-                p.translate(x * size, y * size);
-                p.translate(size / 2, size / 2);
-
-                p.rotate(theta + p.random(-JITTER, JITTER));
-                p.rect(dx, dy, size);
-
-                p.pop();
-            }
+        
+        
+        
+        class Trace {
+            public x: number;
+            public y: number;
+            public life: number;
+            public heading: "left" | "right" | "down";
         }
+        let traces: Array<Trace> = []
+        const popsize = 20;
+        const mortality = 0.035;
+        const viaradius = 10;
+        const bendchance = 0.1;
+
+        // Initialize trace population
+        p.angleMode("degrees");
+        p.push();
+        for (let trace = 0; trace < popsize; trace++){
+            p.push();
+            const x = trace * (WIDTH / popsize);
+            console.log(`x: ${x}, trace: ${trace}`);
+            const yoffset = p.randomGaussian(0, 25);
+            p.translate(x, yoffset);
+            // Start with a via
+            p.circle(0, 0, viaradius)
+            traces.push(
+                {
+                    x: x,
+                    y: yoffset+viaradius/2,
+                    life: 1,
+                    heading: "down"
+                } as Trace
+            );
+            p.pop();
+        }
+        p.pop();
+        
+        const terminate = trace => {
+            p.push();
+            p.translate(trace.x, trace.y);
+            if (trace.heading === 'left'){
+                p.translate(viaradius/2 * p.cos(90-45), viaradius/2 * p.sin(90-45));
+            } else if (trace.heading === 'right') {
+                p.translate(viaradius/2 * p.cos(90+45), viaradius/2 * p.sin(90+45));
+            } else if (trace.heading === 'down') {
+                p.translate(0, viaradius/2);
+            }
+            p.circle(0,0,viaradius);
+            p.pop();
+        };
+
+        while (traces.length > 0){
+            let dead = traces.filter(trace => p.random(0,1) < (mortality / trace.life));
+            let alive = traces.filter(trace => dead.indexOf(trace) == -1);
+            // Terminate dead traces in a via
+            dead.forEach(trace => {
+                terminate(trace);
+            });
+
+            // Alive traces either continue down, bend, or fork
+            alive.forEach((trace, i) => {
+                trace.life *= .91;
+                p.push();
+                p.translate(trace.x, trace.y);
+                const choice = p.random(0,1);
+                // let dist = p.random(size, 2*size);
+                let dist = p.abs(p.randomGaussian(size, size/2));
+                let newx:number = 0, newy:number = 0;
+                if (choice < 1 * bendchance){
+                    trace.heading = "left";
+                    dist *= .5;
+                    newx = dist * p.cos(90-45);
+                    newy = dist * p.sin(90-45);
+                    trace.x += newx;
+                    trace.y += newy;
+                } else if (choice < 2 * bendchance){
+                    trace.heading = "right";
+                    dist *= .5;
+                    newx = dist * p.cos(90+45);
+                    newy = dist * p.sin(90+45);
+                    trace.x += newx;
+                    trace.y += newy;
+                } else {
+                    trace.heading = "down";
+                    trace.y += dist;
+                    newy = dist;
+                }
+                if (i === 0){
+                    console.log(`newx: ${newx} newy: ${newy}`)
+                }
+                p.line(0, 0, newx, newy);
+                if (trace.y + newy > HEIGHT-MARGIN){
+                    terminate(trace);
+                    trace.life = 0.000000000000001;
+                }
+                p.pop();
+            });
+            traces = alive;
+        }
+        /*Pseudocode:
+          - Create initial population of traces
+          - Draw straight down for $DIST
+          - If <rand chance>:
+            - bend 45 left
+            - bend 45 right
+            - fork 45 left
+            - fork 45 right
+            - continue straight
+          - Revert to straight down for <rand total dist>
+          */
+        // for (let y = 0; y < ROWS; y++) {
     };
 
     p.keyPressed = function () {
