ebbd66d22878f90c00c5dabcf027fcce8b5ea5c4
Jeff Wang <jeffawang@gmail.com>
2022-07-09 23:05:44 -0700

    clean up schotter controls a bit

--- src/sketches/Schotter.ts
+++ src/sketches/Schotter.ts
@@ -5,12 +5,11 @@ import { Sketcher, Proxy } from "../sketcher";
 let controls = {
     shift_factor: { type: slider, value: 1.5, min: 0, max: 10 },
     jitter: { type: slider, value: 0.2, min: 0, max: 1 },
-    cols: { type: slider, value: 11, step: 1, max: 50 },
-    rows: { type: slider, value: 15, step: 1, max: 50 },
     greeting: { type: radio, value: "hello", options: ["hello", "bonjour", "hola"] },
-    mygroup: {
+    dimensions: {
         type: group, value: {
-            blah: { type: slider, value: 0.5 }
+            cols: { type: slider, value: 11, step: 1, max: 50 },
+            rows: { type: slider, value: 15, step: 1, max: 50 },
         }
     }
 };
@@ -40,13 +39,13 @@ export const sketcher = new Sketcher({
             p.strokeWeight(2);
             p.rectMode("center");
 
-            const size = (s.params.width - 2 * MARGIN) / u.cols;
+            const size = (s.params.width - 2 * MARGIN) / u.dimensions.cols;
 
             p.translate(MARGIN, MARGIN);
 
-            for (let y = 0; y < u.rows; y++) {
-                for (let x = 0; x < u.cols; x++) {
-                    const yness = y / u.rows + u.jitter;
+            for (let y = 0; y < u.dimensions.rows; y++) {
+                for (let x = 0; x < u.dimensions.cols; x++) {
+                    const yness = y / u.dimensions.rows + u.jitter;
                     const dy = p.pow(p.random(-yness, yness), 3) * size * u.shift_factor;
                     const dx = p.pow(p.random(-yness, yness), 3) * size * u.shift_factor;
                     const theta = p.random(-p.PI, p.PI) * p.pow(yness, 3);
