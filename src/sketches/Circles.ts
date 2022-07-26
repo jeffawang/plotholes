6d3eb6ebede0b7afc671e175052f36475d88353f
Jeff Wang <jeffawang@gmail.com>
2022-07-26 12:53:48 -0700

    circles

--- src/sketches/Circles.ts
+++ src/sketches/Circles.ts
@@ -13,21 +13,28 @@ const controls = {
     type: group,
     value: {
       margin: { type: slider, value: 0, min: 0, max: 100, step: 1 },
-      maxCircles: { type: slider, value: 500, min: 0, max: 5000, step: 1 },
+      maxCircles: { type: slider, value: 1000, min: 0, max: 5000, step: 1 },
       growRate: { type: slider, value: 1, min: 0, max: 10, step: 0.25 },
     },
     collapse: true, // TODO: this doesn't work, fix it
   },
   placement: {
     type: radio,
-    value: 'random',
+    value: 'forces',
     options: ['random', 'forces'],
   },
-  spawnDelay: { type: slider, value: 30, min: 0, max: 1000, step: 0.25 },
+  spawnDelay: { type: slider, value: 2, min: 0, max: 1000, step: 0.25 },
   forces: {
     type: group,
     value: {
-      repulsionFactor: { type: slider, value: 100, min: 0, max: 1000, step: 1 },
+      repulsionFactor: {
+        type: slider,
+        value: 1,
+        min: -10,
+        max: 10,
+        step: 0.25,
+      },
+      maxIntersections: { type: slider, value: 20, min: 0, max: 100, step: 1 },
     },
   },
 };
@@ -53,32 +60,62 @@ export const sketcher = new Sketcher({
     };
 
     class Circle {
-      center: p5.Vector;
-      r: number;
+      pos: p5.Vector;
+      vel: p5.Vector;
+
+      radius: number;
       growing: boolean;
+
+      color: p5.Color;
+
+      lifetimeIntersections: number;
+
       constructor(center: p5.Vector) {
-        this.center = center;
-        this.r = 0;
+        this.pos = center;
+        this.radius = 0;
         this.growing = true;
+        this.vel = p.createVector(0, 0);
+        this.color = p.color(0);
+        this.lifetimeIntersections = 0;
       }
 
       show() {
-        p.circle(this.center.x, this.center.y, this.r * 2);
+        p.push();
+        p.fill(this.color);
+        p.circle(this.pos.x, this.pos.y, this.radius * 2);
+        p.pop();
       }
 
-      tryGrow() {
+      update() {
         if (this.growing) {
-          this.r += u.circles.growRate;
+          this.radius += u.circles.growRate;
         }
+
+        const endHue = p.map(this.pos.x, 0, p.width, 0, 255);
+        this.color = p.lerpColor(
+          p.color(0),
+          p.color(endHue, 56, 125),
+          p.map(this.radius, 0, 50, 0, 1)
+        );
+        this.pos.add(this.vel);
+        this.vel.mult(0);
       }
 
       intersects(other: Circle): boolean {
-        const d = p5.Vector.dist(this.center, other.center);
-        return d < this.r + other.r + strokeWeight;
+        const d = p5.Vector.dist(this.pos, other.pos);
+        const intersecting = d < this.radius + other.radius + strokeWeight;
+        if (intersecting) {
+          this.lifetimeIntersections += 1;
+        }
+        return intersecting;
       }
     }
 
-    const circles: Circle[] = new Array(u.circles.maxCircles);
+    function sdf(circle: Circle, other: Circle) {
+      return circle.pos.dist(other.pos) - (circle.radius + other.radius);
+    }
+
+    const allCircles: Circle[] = new Array(u.circles.maxCircles);
     let numCircles: number;
 
     const strokeWeight = 5;
@@ -88,11 +125,19 @@ export const sketcher = new Sketcher({
       numCircles = 0;
     };
 
+    function randomPoint() {
+      return p.createVector(
+        p.random(-u.circles.margin, p.width + u.circles.margin),
+        p.random(-u.circles.margin, p.height + u.circles.margin)
+      );
+    }
+
     p.draw = function () {
       p.background(colors.bg);
       p.stroke(colors.fg);
       p.noFill();
       p.strokeWeight(strokeWeight);
+      p.colorMode(p.HSB);
 
       // option 1: choose random points until not inside a circle
       // option 2: choose random point.
@@ -109,22 +154,24 @@ export const sketcher = new Sketcher({
 
       function randomChoice(): p5.Vector | null {
         for (let i = 0; i < 1000; i++) {
-          const c = p.createVector(
-            p.random(-u.circles.margin, p.width + u.circles.margin),
-            p.random(-u.circles.margin, p.height + u.circles.margin)
-          );
+          const c = randomPoint();
 
           if (
-            circles
+            allCircles
               .slice(0, numCircles)
               .every(
-                (circle) => circle.center.dist(c) > circle.r + strokeWeight
+                (circle) => circle.pos.dist(c) > circle.radius + strokeWeight
               )
           ) {
             return c;
           }
         }
         return null;
+      }
+
+      function forces(): p5.Vector {
+        const c = randomPoint();
+        return c;
       }
 
       // add a circle if we can
@@ -139,26 +186,54 @@ export const sketcher = new Sketcher({
             c = randomChoice();
             break;
           case 'forces':
-            // c = forces();
+            c = forces();
             break;
         }
         if (c != null) {
-          circles[numCircles] = new Circle(c);
+          allCircles[numCircles] = new Circle(c);
           numCircles += 1;
         } else {
           console.log('failed to place a new circle, sorry buddy!');
         }
       }
 
-      for (let i = 0; i < numCircles; i++) {
-        for (let j = i + 1; j < numCircles; j++) {
-          if (circles[i].intersects(circles[j])) {
-            circles[i].growing = false;
-            circles[j].growing = false;
+      const circles = allCircles.slice(0, numCircles);
+      for (const [i, circle] of circles.entries()) {
+        if (u.placement == 'random') {
+          for (const other of circles.slice(i + 1)) {
+            if (circle.intersects(other)) {
+              circle.growing = false;
+              other.growing = false;
+            }
+          }
+        } else if (u.placement == 'forces') {
+          let intersections = 0;
+          for (const other of circles) {
+            if (circle === other) {
+              continue;
+            }
+            if (circle.intersects(other)) {
+              intersections += 1;
+              const forceDirection = p5.Vector.sub(
+                other.pos,
+                circle.pos
+              ).normalize();
+              const sd = sdf(circle, other) * u.forces.repulsionFactor;
+              if (sd < 0) {
+                forceDirection.mult(sd);
+                circle.vel.set(forceDirection);
+              }
+            }
+          }
+          if (circle.lifetimeIntersections > u.forces.maxIntersections) {
+            // circle.color = p.color('red');
+            circle.growing = false;
           }
         }
-        circles[i].tryGrow();
-        circles[i].show();
+      }
+      for (const circle of circles) {
+        circle.update();
+        circle.show();
       }
     };
   },
