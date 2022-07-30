6d71a7b9fc7025df29dd4aafbee68d99e634df38
Jeff Wang <jeffawang@gmail.com>
2022-07-29 17:01:16 -0700

    sort sketch: add gaussian distribution radio button

--- src/sketches/Sort.ts
+++ src/sketches/Sort.ts
@@ -2,8 +2,10 @@ import p5 from 'p5';
 import {
   button,
   checkbox,
+  radio,
   slider,
   UniformButton,
+  UniformRadio,
   UniformSlider,
 } from '../components/Controls/UniformControls';
 import { Sketcher, Uniforms } from '../sketcher';
@@ -12,6 +14,11 @@ const controls = {
   count: { type: slider, value: 50, min: 1, max: 100, step: 1 },
   step: { type: button },
   autoplay: { type: checkbox, value: false },
+  distribution: {
+    type: radio,
+    value: 'uniform',
+    options: ['uniform', 'gaussian'],
+  },
 };
 
 export const sketcher = new Sketcher({
@@ -39,9 +46,11 @@ export const sketcher = new Sketcher({
     let len: number;
 
     function init() {
-      numbers = Array.from({ length: u.count }, () =>
-        Math.floor(Math.random() * 100)
-      );
+      const rand =
+        u.distribution == 'uniform'
+          ? p.random.bind(p)
+          : p.randomGaussian.bind(p);
+      numbers = Array.from({ length: u.count }, () => Math.floor(rand() * 100));
       idx = 0;
       len = numbers.length;
     }
@@ -68,6 +77,7 @@ export const sketcher = new Sketcher({
       idx++;
     }
 
+    (s.params.controls.distribution as UniformRadio).onChange = init;
     (s.params.controls.count as UniformSlider).onChange = init;
     (s.params.controls.step as UniformButton).onClick = bubbleStep;
 
@@ -78,12 +88,18 @@ export const sketcher = new Sketcher({
       p.strokeWeight(1);
       p.fill(200);
 
-      const max = Math.max(...numbers);
+      const min = p.min(numbers);
+      const max = p.max(numbers);
+      const biggest = p.max(p.abs(min), max);
       for (let i = 0; i < numbers.length; i++) {
         const n = numbers[i];
         const x = p.map(i, 0, numbers.length, p.width * 0.05, p.width * 0.95);
-        const bottom = p.height * 0.95;
-        const height = p.map(n, 0, max, 0, p.height * 0.9);
+        const bottom =
+          u.distribution == 'uniform' ? p.height * 0.95 : p.height * 0.5;
+        const height =
+          u.distribution == 'uniform'
+            ? p.map(n, 0, max, 0, p.height * 0.9)
+            : p.map(n, -biggest, biggest, -p.height * 0.45, p.height * 0.45);
         // p.line(x, bottom, x, bottom - height);
         p.push();
         if (i == idx) {
